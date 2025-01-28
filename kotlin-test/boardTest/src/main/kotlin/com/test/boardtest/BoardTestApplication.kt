b7299417fef830e0efb25ea07a6bc134ed6346e6
CodeDDAGGI <qudrb1132@naver.com>
2025-01-28 22:46:46 +0900

    Feat : "코틀린 보드 연습"

---
+++ kotlin-test/boardTest/src/main/kotlin/com/test/boardtest/BoardTestApplication.kt
@@ -0,0 +1,11 @@
+package com.test.boardtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardTestApplication>(*args)
+}
